5877449949337d1b13457c078f61b9387d44970c
Ross Savage <ross.savage@agrista.com>
2013-09-09 13:28:27 +0200

    Migration of mobile-framework to angularjs 1.2.0 rc2

--- dist/core/js/navigationModule.js
+++ dist/core/js/navigationModule.js
@@ -1,106 +1,69 @@
 'use strict';
 
-define(['angular', 'core/utilityModule'], function () {
-    var module = angular.module('navigationModule', ['utilityModule']);
+define(['angular', 'angular-animate', 'core/utilityModule'], function () {
+    var module = angular.module('navigationModule', ['ngAnimate', 'utilityModule']);
 
-    module.controller('NavigationController', ['$scope', 'navigationService', function ($scope, navigationService) {
-        $scope.getTransition = navigationService.getCurrentTransition;
-
-        $scope.menu = {
-            show: false,
-            toggle: function () {
-                $scope.menu.show = !$scope.menu.show;
-
-                if($scope.menu.show === true) {
-                    $scope.menu.items = navigationService.menu();
-                }
-            },
-            title: 'Agrista',
-            click: function(index) {
-                $scope.menu.show = false;
-
-                var item = $scope.menu.items[index];
-
-                if (typeof item.click === 'function') {
-                    item.click();
-                }
-            }
-        };
-    }]);
-
-    module.service('navigationService', ['$location', function ($location) {
-        var _transitions = {
-            slide: {
-                transitionIn: {enter: 'animate slide-enter', leave: 'animate slide-leave'},
-                transitionOut: {enter: 'animate slide-reverse-enter', leave: 'animate slide-reverse-leave'}
-            },
-            modal: {
-                transitionIn: {enter: 'animate modal-enter', leave: 'animate modal-leave'},
-                transitionOut: {enter: 'animate modal-reverse-enter', leave: 'animate modal-reverse-leave'}
-            }
-        }
-
+    module.service('navigationService', ['$rootScope', '$location', function ($rootScope, $location) {
         var _transition = undefined;
-        var _menu = [];
 
         return {
-            addTransition: function (name, data) {
-                _transitions[name] = data;
-            },
-            getCurrentTransition: function () {
-                return _transition;
-            },
-            go: function (url, type, reverse) {
-                type = type || 'slide';
+            go: function (url, type, pop) {
+                _transition = {
+                    url: url,
+                    type: type,
+                    push: !pop
+                };
 
-                _transition = (reverse !== true ? _transitions[type].transitionIn : _transitions[type].transitionOut);
+                $rootScope.$broadcast('navigation', _transition);
 
-                $location.path(url);
-            },
-            menu: function(items) {
-                if(items !== undefined) {
-                    _menu = items;
-                }
-
-                return _menu;
+                $location.url(url);
             }
         }
     }]);
 
-    module.directive('navigationMenu', function () {
+    module.directive('pageMenu', function () {
         return {
             restrict: 'E',
             replace: true,
-            template: '<div id="nav-menu" ng-class="{\'show-menu\': menu.show === true}">\n    <div class="navbar">\n        <div class="container">\n            <p class="navbar-text">{{ menu.title }}</p>\n        </div>\n    </div>\n    <nav class="list-container">\n        <ul class="nav">\n            <li ng-repeat="item in menu.items">\n                <a ng-click="menu.click($index)" ng-class="{active: item.active}" stop-propagation>{{ item.title }}</a>\n            </li>\n        </ul>\n    </nav>\n</div>'
+            template: '<div class="page-menu" ng-class="{\'show-menu\': menu.show === true}">\n    <div class="navbar">\n        <div class="container">\n            <p class="navbar-text">{{ menu.title }}</p>\n        </div>\n    </div>\n    <nav class="list-container">\n        <ul class="nav">\n            <li ng-repeat="item in menu.items">\n                <a ng-click="menu.click($index)" ng-class="{active: item.active}" stop-propagation>{{ item.title }}</a>\n            </li>\n        </ul>\n    </nav>\n</div>\n'
         };
     });
 
-    module.directive('navigationPage', function () {
+    module.directive('pageToolbar', function () {
         return {
             restrict: 'E',
             replace: true,
-            template: '<div id="nav-page" class="page-slide" ng-class="{\'show-menu\': menu.show === true}">\n    <div class="container" ng-transclude></div>\n</div>',
-            transclude: true
+            transclude: true,
+            template: '<div class="toolbar">\n    <div class="container" ng-transclude></div>\n</div>'
         };
     });
 
-    module.directive('toolBar', function () {
+    module.directive('pageContainer', function () {
         return {
             restrict: 'E',
             replace: true,
-            template: '<div class="toolbar page-slide" ng-class="{\'show-menu\': menu.show === true}">\n    <div class="container" ng-transclude></div>\n</div>',
-            transclude: true
+            transclude: true,
+            template: '<div class="page-container" ng-transclude></div>'
+        };
+    });
+
+    module.directive('pageContent', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: true,
+            template: '<div class="page-content" ng-transclude></div>'
         };
     });
 
     /**
-     * Setup the navigation-bar directive. Directive injects the navigation-bar template and provides a button interface.
+     * Setup the page-navbar directive. Directive injects the page-navbar template and provides a button interface.
      * When no navigate-left or navigate-right function is provided then the button is hidden.
-     * <navigation-bar title="Navbar title"
+     * <page-navbar title="Navbar title"
      *         left-button="{icon: 'remove'}"
      *         navigate-left="navigateLeftFunction"
      *         right-button="{icon: 'ok', title: 'Save'}"
-     *         navigate-right="navigateRightFunction"></navigation-bar>
+     *         navigate-right="navigateRightFunction"></page-navbar>
      *
      * @param title {string} The navigation bar title
      * @param left-button {object} Left button chrome settings {icon: string, title: string}
@@ -108,7 +71,7 @@ define(['angular', 'core/utilityModule'], function () {
      * @param navigate-left {function} Function to call on left button click
      * @param navigate-right {function} Function to call on right button click
      */
-    module.directive('navigationBar', function () {
+    module.directive('pageNavbar', function () {
         return {
             restrict: 'E',
             scope: {
@@ -121,7 +84,7 @@ define(['angular', 'core/utilityModule'], function () {
             },
             replace: true,
             transclude: true,
-            template: '<div class="navbar navbar-inverse page-slide" ng-class="{\'show-menu\': menuShown === true}" ng-transclude>\n    <div class="container">\n        <div class="pull-left" ng-show="showLeftButton()">\n            <div class="btn navbar-btn btn-clear" ng-click="triggerLeftNav()">\n                <i class="glyphicon glyphicon-{{leftButton.icon}}" ng-show="leftButton.icon"></i>\n                <span ng-show="leftButton.title">&nbsp;{{leftButton.title}}</span>\n            </div>\n        </div>\n        <div class="navbar-title"><p class="navbar-text">{{ title }}</p></div>\n        <div class="pull-right" ng-show="showRightButton()">\n            <div class="btn navbar-btn btn-primary" ng-click="triggerRightNav()">\n                <i class="glyphicon glyphicon-{{rightButton.icon}}" ng-show="rightButton.icon"></i>\n                <span ng-show="rightButton.title">&nbsp;{{rightButton.title}}</span>\n            </div>\n        </div>\n    </div>\n</div>\n',
+            template: '<div class="navbar navbar-inverse page-slide" ng-class="{\'show-menu\': menuShown === true}">\n    <div class="container">\n        <div class="pull-left" ng-show="showLeftButton()">\n            <div class="btn navbar-btn btn-clear" ng-click="triggerLeftNav()">\n                <i class="glyphicon glyphicon-{{leftButton.icon}}" ng-show="leftButton.icon"></i>\n                <span ng-show="leftButton.title">&nbsp;{{leftButton.title}}</span>\n            </div>\n        </div>\n        <div class="navbar-title"><p class="navbar-text">{{ title }}</p></div>\n        <div class="pull-right" ng-show="showRightButton()">\n            <div class="btn navbar-btn btn-primary" ng-click="triggerRightNav()">\n                <i class="glyphicon glyphicon-{{rightButton.icon}}" ng-show="rightButton.icon"></i>\n                <span ng-show="rightButton.title">&nbsp;{{rightButton.title}}</span>\n            </div>\n        </div>\n    </div>\n</div>\n',
             controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
                 $scope.triggerLeftNav = function() {
                     $scope.navigateLeft();
