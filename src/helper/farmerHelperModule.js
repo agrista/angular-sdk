70c404b6b6fe5c5c7b107a605d8a61e27006df16
Wiliam Lee <william.lee@agrista.com>
2014-03-13 14:26:12 +0100

    updated legal entity types in farmer helper.

--- src/helper/farmerHelperModule.js
+++ src/helper/farmerHelperModule.js
@@ -28,7 +28,7 @@ sdkHelperFarmerApp.factory('legalEntityHelper', [function() {
         };
     };
 
-    var _legalEntityTypes = ['Close Corporation', 'Co-operation', 'Incorporated Company', 'Private Company', 'Partnership', 'Public Company', 'State Owned Company', 'Sole Proprietor', 'Trust', 'Association', 'Government', 'Individual', 'Strategic Business Unit', 'Limited', 'Unknown', 'Other'];
+    var _legalEntityTypes = ['Individual', 'Sole Proprietor', 'Close Corporation', 'Trust', 'Party Limited Company', 'Co-operative', 'Partnership', 'State Owned Company', 'Association'];
 
     var _enterpriseTypes = {
         'Field Crops': ['Barley', 'Cabbage', 'Canola', 'Chicory', 'Citrus (Hardpeel)', 'Cotton', 'Cow Peas', 'Dry Bean', 'Dry Grapes', 'Dry Peas', 'Garlic', 'Grain Sorghum', 'Green Bean', 'Ground Nut', 'Hybrid Maize Seed', 'Lentils', 'Lucerne', 'Maize (Fodder)', 'Maize (Green)', 'Maize (Seed)', 'Maize (White)', 'Maize (Yellow)', 'Oats', 'Onion', 'Onion (Seed)', 'Popcorn', 'Potato', 'Pumpkin', 'Rye', 'Soya Bean', 'Sugar Cane', 'Sunflower', 'Sweetcorn', 'Tobacco', 'Tobacco (Oven dry)', 'Tomatoes', 'Watermelon', 'Wheat'],
