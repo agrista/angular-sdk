ce1324006be4c25fc9857842f238518922f24f98
Wiliam Lee <william.lee@agrista.com>
2014-03-17 19:21:12 +0100

    fixed pip calling.

--- src/interface/mapModule.js
+++ src/interface/mapModule.js
@@ -1724,7 +1724,8 @@ sdkInterfaceMapApp.directive('mapbox', ['$rootScope', '$http', 'mapboxService', 
         var _this = this;
 
         if (_this._editing == false) {
-            $http.get('/api/geo/portion-polygon/' + e.latlng.lat + '/' + e.latlng.lng)
+            var params = '?x=' + e.latlng.lng + '&y=' + e.latlng.lat;
+            $http.get('/api/geo/portion-polygon/' + params)
                 .success(function (portion) {
                     _this._mapboxServiceInstance.removeGeoJSONLayer(_this._editableLayer);
                     _this._mapboxServiceInstance.addGeoJSON(_this._editableLayer, portion.position, _this._optionSchema, {featureId: portion.sgKey});
@@ -1740,7 +1741,8 @@ sdkInterfaceMapApp.directive('mapbox', ['$rootScope', '$http', 'mapboxService', 
         var _this = this;
 
         if (_this._editing == false) {
-            $http.get('/api/geo/portion-polygon/' + e.latlng.lat + '/' + e.latlng.lng)
+            var params = '?x=' + e.latlng.lng + '&y=' + e.latlng.lat;
+            $http.get('/api/geo/portion-polygon/' + params)
                 .success(function (portion) {
                     _this._mapboxServiceInstance.addGeoJSON(_this._editableLayer, portion.position, _this._optionSchema, {featureId: portion.sgKey, portion: portion});
 
