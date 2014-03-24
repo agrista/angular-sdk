5c75c7c31169df7edaf65abac227240bcb753174
Ross Savage <ross.savage@agrista.com>
2014-03-24 10:27:49 +0100

    Rebuild dist files

--- dist/agrista-mobile-sdk.js
+++ dist/agrista-mobile-sdk.js
@@ -4136,6 +4136,9 @@ sdkInterfaceNavigiationApp.provider('navigationService', function() {
     var _registeredApps = {};
     var _groupedApps = [];
 
+    var _leftButtons = [];
+    var _rightButtons = [];
+
     var _groupOrder = {
         'Favourites': 1,
         'Assets': 2,
@@ -4263,6 +4266,32 @@ sdkInterfaceNavigiationApp.provider('navigationService', function() {
                 }
 
                 return _footerText;
+            },
+
+            /*
+             * Buttons
+             */
+            leftButtons: function (/**Array=*/buttons) {
+                if (buttons) {
+                    if ((buttons instanceof Array) === false) {
+                        buttons = [buttons];
+                    }
+
+                    _leftButtons = buttons;
+                }
+
+                return _leftButtons;
+            },
+            rightButtons: function (/**Array=*/buttons) {
+                if (buttons) {
+                    if ((buttons instanceof Array) === false) {
+                        buttons = [buttons];
+                    }
+
+                    _rightButtons = buttons;
+                }
+
+                return _rightButtons;
             }
         }
     }];
