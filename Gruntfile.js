0144163a29eba4ab4787fc8f712e0cefbbff8f2f
Ross Savage <ross.savage@agrista.com>
2014-03-31 13:01:48 +0200

    Add grunt-sloc

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,11 +18,21 @@ module.exports = function(grunt) {
                 }
             }
         },
-        connect: {
-            server: {
+        sloc: {
+            client: {
                 options: {
-                    port: 8000,
-                    keepalive: true
+                    torelant: true
+                },
+                files: {
+                    'dist': ['agrista-sdk.js']
+                }
+            },
+            mobile: {
+                options: {
+                    torelant: true
+                },
+                files: {
+                    'dist': ['agrista-mobile-sdk.js']
                 }
             }
         }
@@ -30,8 +40,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-sloc');
 
     grunt.registerTask('default', ['concat', 'uglify']);
-    grunt.registerTask('server', ['default', 'connect']);
 };
