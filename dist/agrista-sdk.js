cad0b6b55733d18fb3a5f3d1a1413e96da5d595f
Wiliam Lee <william.lee@agrista.com>
2014-05-02 17:52:17 +0200

    added jSignature directory to sdk

--- dist/agrista-sdk.js
+++ dist/agrista-sdk.js
@@ -1636,6 +1636,38 @@ skdUtilitiesApp.factory('localStore', ['$cookieStore', '$window', function ($coo
     }
 }]);
 
+skdUtilitiesApp.directive('signature', ['$compile', function ($compile) {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div class="panel panel-default signature"><div class="panel-heading">{{ title }}<div class="btn btn-default btn-sm pull-right" ng-click="reset()">Clear</div></div></div>',
+        scope: {
+            onsigned: '=',
+            name: '@',
+            title: '@'
+        },
+        link: function (scope, element, attrs) {
+            var sigElement = $compile('<div class="panel-body signature-body"></div>')(scope);
+
+            element.append(sigElement);
+
+            scope.reset = function() {
+                sigElement.jSignature('reset');
+
+                scope.onsigned(attrs.name, null);
+            };
+
+            sigElement.jSignature({
+                'width': attrs.width,
+                'height': attrs.height,
+                'showUndoButton': false});
+
+            sigElement.bind('change', function() {
+                scope.onsigned(attrs.name, sigElement.jSignature('getData', 'svgbase64'));
+            });
+        }
+    };
+}]);
 var sdkHelperAssetApp = angular.module('ag.sdk.helper.asset', ['ag.sdk.helper.farmer']);
 
 sdkHelperAssetApp.factory('assetHelper', ['$filter', 'landUseHelper', function($filter, landUseHelper) {
