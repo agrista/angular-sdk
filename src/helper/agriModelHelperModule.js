21f0a6b7353ba3bb1ea4a17961d5029ce32fd9c7
Ross Savage <ross.savage@agrista.com>
2014-05-30 10:43:06 +0200

    Add model types and total calculation to agriModelHelper

--- src/helper/agriModelHelperModule.js
+++ src/helper/agriModelHelperModule.js
@@ -4,13 +4,44 @@ sdkHelperAgriModelApp.factory('agriModelHelper', [function() {
     var _listServiceMap = function (item) {
         return {
             title: item.name,
-            subtitle: item.commodityType + ' in ' + item.region
+            subtitle: item.commodityType + (item.region && item.region.properties ? ' in ' + item.region.properties.name : '')
         }
     };
 
+    var _modelTypes = {
+        crop: 'Crop',
+        livestock: 'Livestock'
+    };
+
     return {
-        listServiceMap: function() {
+        listServiceMap: function () {
             return _listServiceMap;
+        },
+        getModelType: function (type) {
+            return _modelTypes[type] || '';
+        },
+
+        calculateTotals: function (agriModel) {
+            var income = agriModel.data.income = agriModel.data.income || {};
+            var expenses = agriModel.data.expenses = agriModel.data.expenses || [];
+            var total = agriModel.data.total = agriModel.data.total || {};
+
+            if (isNaN(income.yield) == false && isNaN(income.price) == false) {
+                total.income = income.yield * income.price;
+            }
+
+            total.expenses = 0;
+
+            angular.forEach(expenses, function (type) {
+                angular.forEach(type, function (subtype) {
+                    total.expenses += (subtype.cost || 0);
+                });
+            });
+
+            total.profit = total.income - total.expenses;
+
+            return agriModel;
+
         }
     }
 }]);
