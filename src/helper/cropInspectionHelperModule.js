7f7f4c684decfce31efe042df7fd9967fa30566d
Ross Savage <ross.savage@agrista.com>
2014-06-13 18:12:28 +0200

    Add inspection and policy types to cropInspectionHelper

--- src/helper/cropInspectionHelperModule.js
+++ src/helper/cropInspectionHelperModule.js
@@ -1,25 +1,42 @@
 var sdkHelperCropInspectionApp = angular.module('ag.sdk.helper.crop-inspection', []);
 
 sdkHelperCropInspectionApp.factory('cropInspectionHelper', [function() {
-    var _purposes = ['Arrears', 'Building line restriction', 'Consolidation', 'Covering Bond', 'Expropration', 'Extention of section/plan', 'Further Advance', 'New Loan', 'Notarial agreement', 'Opening of sectional title register/township', 'Progress Payment', 'Property in Possession (PIP)', 'Rectification transfer', 'Release of portions/units/sections', 'Removal of restrictive conditions', 'Replacement value', 'Rezoning', 'Securities', 'Servitude', 'Special consent', 'Subdivision', 'Township establishment', 'Other'];
+    var _policyTypes = {
+        'hail': 'Hail',
+        'multi peril': 'Multi Peril'
+    };
 
-    var _priorities = [{
-        key: 'Priority 1',
-        value: 1
-    }, {
-        key: 'Priority 2',
-        value: 2
-    }, {
-        key: 'Priority 3',
-        value: 3
-    }];
+    var _inspectionTypes = {
+        emergence: 'Emergence',
+        hail: 'Hail',
+        harvest: 'Harvest',
+        preharvest: 'Pre Harvest',
+        progress: 'Progress'
+    };
+
+    var _policyInspections = {
+        'hail': {
+            hail: _inspectionTypes.hail
+        },
+        'multi peril': _inspectionTypes
+    };
 
     return {
-        purposes: function() {
-            return _purposes;
+        inspectionTypes: function () {
+            return _inspectionTypes;
         },
-        priorities: function() {
-            return _priorities;
+        policyTypes: function () {
+            return _policyTypes;
+        },
+        policyInspectionTypes: function (policyType) {
+            return _policyInspections[policyType] || {};
+        },
+
+        getPolicyTitle: function (type) {
+            return _policyTypes[type] || '';
+        },
+        getInspectionTitle: function (type) {
+            return _inspectionTypes[type] || '';
         }
     }
 }]);
