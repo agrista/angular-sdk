8666eaba2967efc7f519a232db7c85cd950b690b
Ross Savage <ross.savage@agrista.com>
2014-06-23 08:49:29 +0200

    Add growth stages, seed types to crop inspection helper

--- src/helper/cropInspectionHelperModule.js
+++ src/helper/cropInspectionHelperModule.js
@@ -1,9 +1,36 @@
 var sdkHelperCropInspectionApp = angular.module('ag.sdk.helper.crop-inspection', ['ag.sdk.helper.document']);
 
 sdkHelperCropInspectionApp.factory('cropInspectionHelper', ['documentHelper', function(documentHelper) {
-    var _policyTypes = {
-        'hail': 'Hail',
-        'multi peril': 'Multi Peril'
+    var _growthStageTable = [
+        ['V0', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', 'R10', 'R11', 'R12', 'R13', 'R14'],
+        ['V0', 'V1', 'V2', 'V3', 'V4', 'V5', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6'],
+        ['V0', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9'],
+        ['V0', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', 'R10', 'R11', 'R12'],
+        ['V0', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8'],
+        ['V0', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9'],
+        ['V0', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', 'R10', 'R11', 'R12', 'R13', 'R14', 'R15', 'R16', 'R17', 'R18'],
+        ['V0', 'V1', 'V2', 'V3', 'V4', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6'],
+        ['V0', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6']
+    ];
+
+    var _growthStageCrops = {
+        'Barley': _growthStageTable[1],
+        'Bean': _growthStageTable[5],
+        'Bean (Broad)': _growthStageTable[5],
+        'Bean (Dry)': _growthStageTable[5],
+        'Bean (Sugar)': _growthStageTable[5],
+        'Bean (Green)': _growthStageTable[5],
+        'Bean (Kidney)': _growthStageTable[5],
+        'Canola': _growthStageTable[7],
+        'Cotton': _growthStageTable[6],
+        'Grain Sorghum': _growthStageTable[3],
+        'Maize': _growthStageTable[0],
+        'Maize (White)': _growthStageTable[0],
+        'Maize (Yellow)': _growthStageTable[0],
+        'Soybean': _growthStageTable[2],
+        'Sunflower': _growthStageTable[4],
+        'Wheat': _growthStageTable[1],
+        'Wheat (Durum)': _growthStageTable[1]
     };
 
     var _inspectionTypes = {
@@ -12,6 +39,21 @@ sdkHelperCropInspectionApp.factory('cropInspectionHelper', ['documentHelper', fu
         harvest: 'Harvest Inspection',
         preharvest: 'Pre Harvest Inspection',
         progress: 'Progress Inspection'
+    };
+
+    var _seedTypeTable = [
+        ['Maize Commodity', 'Maize Hybrid', 'Maize Silo Fodder']
+    ];
+
+    var _seedTypes = {
+        'Maize': _seedTypeTable[0],
+        'Maize (White)': _seedTypeTable[0],
+        'Maize (Yellow)': _seedTypeTable[0]
+    };
+
+    var _policyTypes = {
+        'hail': 'Hail',
+        'multi peril': 'Multi Peril'
     };
 
     var _policyInspections = {
@@ -48,11 +90,21 @@ sdkHelperCropInspectionApp.factory('cropInspectionHelper', ['documentHelper', fu
             return _policyInspections[policyType] || {};
         },
 
-        getPolicyTitle: function (type) {
-            return _policyTypes[type] || '';
+        getGrowthStages: function (crop) {
+            return _growthStageCrops[crop] || _growthStageTable[0];
+        },
+        getSeedTypes:function (crop) {
+            return _seedTypes[crop];
         },
         getInspectionTitle: function (type) {
             return _inspectionTypes[type] || '';
+        },
+        getPolicyTitle: function (type) {
+            return _policyTypes[type] || '';
+        },
+
+        hasSeedTypes: function (crop) {
+            return _seedTypes[crop] !== undefined;
         }
     }
 }]);
