d55de30da9748ca99d5b5e3d92d000779e5c2a31
Wiliam Lee <william.lee@agrista.com>
2014-08-19 21:12:08 +0200

    formatted the geoJson first before validation and rendering.

--- dist/agrista-sdk.js
+++ dist/agrista-sdk.js
@@ -3235,6 +3235,31 @@ sdkInterfaceMapApp.factory('geoJSONHelper', function () {
 
             return this;
         },
+        formatGeoJson: function (geoJson, toType) {
+            //todo: maybe we can do the geoJson formation to make it standard instead of doing the validation.
+            if(toType.toLowerCase() == 'point') {
+                switch (geoJson && geoJson.type && geoJson.type.toLowerCase()) {
+                    // type of Feature
+                    case 'feature':
+                        if(geoJson.geometry && geoJson.geometry.type && geoJson.geometry.type == 'Point') {
+                            console.log(geoJson.geometry);
+                            return geoJson.geometry;
+                        }
+                        break;
+                    // type of FeatureCollection
+                    case 'featurecollection':
+                        break;
+                    // type of GeometryCollection
+                    case 'geometrycollection':
+                        break;
+                    // type of Point, MultiPoint, LineString, MultiLineString, Polygon, MultiPolygon
+                    default:
+                        break;
+                }
+            }
+
+            return geoJson;
+        },
         validGeoJson: function (geoJson, typeRestriction) {
             var validate = true;
             if(!geoJson || geoJson.type == undefined || typeof geoJson.type != 'string' || (typeRestriction && geoJson.type.toLowerCase() != typeRestriction)) {
