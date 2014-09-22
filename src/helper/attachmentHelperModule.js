ab33d7cb06545b311d3591bee0d64ac29c030cb3
Ross Savage <ross.savage@agrista.com>
2014-09-22 16:51:25 +0200

    refactor(attachmentHelper): change from factory to provider to allow defaultImage to be set at config

--- src/helper/attachmentHelperModule.js
+++ src/helper/attachmentHelperModule.js
@@ -1,29 +1,39 @@
 var sdkHelperAttachmentApp = angular.module('ag.sdk.helper.attachment', ['ag.sdk.library']);
 
-sdkHelperAttachmentApp.factory('attachmentHelper', ['underscore', function (underscore) {
-    var _getResizedAttachment = function (attachments, size) {
-        if (attachments !== undefined) {
-            if ((attachments instanceof Array) == false) {
-                attachments = [attachments];
-            }
-
-            return underscore.chain(attachments)
-                .filter(function (attachment) {
-                    return (attachment.sizes !== undefined && attachment.sizes[size] !== undefined);
-                }).map(function (attachment) {
-                    return attachment.sizes[size].src;
-                }).last().value();
-        }
-
-        return attachments;
+sdkHelperAttachmentApp.provider('attachmentHelper', ['underscore', function (underscore) {
+    var _options = {
+        defaultImage: 'img/camera.png'
     };
 
-    return {
-        getSize: function (attachments, size) {
-            return _getResizedAttachment(attachments, size);
-        },
-        getThumbnail: function (attachments) {
-            return _getResizedAttachment(attachments, 'thumb');
-        }
+    this.config = function (options) {
+        _options = underscore.defaults(options || {}, _options);
+    };
+
+    this.$get = function () {
+        var _getResizedAttachment = function (attachments, size) {
+            if (attachments !== undefined) {
+                if ((attachments instanceof Array) == false) {
+                    attachments = [attachments];
+                }
+
+                return underscore.chain(attachments)
+                    .filter(function (attachment) {
+                        return (attachment.sizes !== undefined && attachment.sizes[size] !== undefined);
+                    }).map(function (attachment) {
+                        return attachment.sizes[size].src;
+                    }).last().value();
+            }
+
+            return attachments;
+        };
+
+        return {
+            getSize: function (attachments, size) {
+                return _getResizedAttachment(attachments, size) || _options.defaultImage;
+            },
+            getThumbnail: function (attachments) {
+                return _getResizedAttachment(attachments, 'thumb') || _options.defaultImage;
+            }
+        };
     };
 }]);
