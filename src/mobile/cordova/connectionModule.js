9d75a6c1a441f2e078af851bd0034de7a28effae
Ross Savage <ross.savage@agrista.com>
2014-10-23 13:28:23 +0200

    refactor(connectionService): rework connection watcher

--- src/mobile/cordova/connectionModule.js
+++ src/mobile/cordova/connectionModule.js
@@ -1,20 +1,16 @@
 var cordovaConnectionApp = angular.module('ag.mobile-sdk.cordova.connection', []);
 
 cordovaConnectionApp.factory('connectionService', ['$timeout', function ($timeout) {
-    var _onConnectionChangeList = [];
+    var _watchConnectionList = [];
     var _lastConnectionType = undefined;
 
     var _updateConnection = function () {
         if (_lastConnectionType !== navigator.connection.type) {
             _lastConnectionType = navigator.connection.type;
 
-            for (var i = _onConnectionChangeList.length - 1; i >= 0; i--) {
-                if (_onConnectionChangeList[i] !== undefined) {
-                    _onConnectionChangeList[i](_lastConnectionType);
-                } else {
-                    _onConnectionChangeList.splice(i, 1);
-                }
-            }
+            angular.forEach(_watchConnectionList, function (watcher) {
+                watcher(_lastConnectionType);
+            });
         }
 
         $timeout(_updateConnection, 10000);
@@ -23,13 +19,13 @@ cordovaConnectionApp.factory('connectionService', ['$timeout', function ($timeou
     _updateConnection();
 
     return {
-        onConnectionChange: function (onChangeCb) {
-            if (typeof onChangeCb === 'function') {
-                _onConnectionChangeList.push(onChangeCb);
+        watchConnection: function (callback) {
+            if (typeof callback === 'function') {
+                _watchConnectionList.push(callback);
             }
         },
         isOnline: function () {
-            return (navigator.connection.type !== Connection.NONE);
+            return (navigator.connection.type !== Connection.NONE && navigator.connection.type !== Connection.UNKNOWN);
         },
         isMobile: function () {
             return (navigator.connection.type === Connection.CELL ||
