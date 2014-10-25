9998f3c0ce69fb7546ebc53cf5489a7806c9cd68
Ross Savage <ross.savage@agrista.com>
2014-10-25 22:13:24 +0200

    feat(resizeImageService): implement service to resize, crop & center images

--- src/helper/attachmentHelperModule.js
+++ src/helper/attachmentHelperModule.js
@@ -37,3 +37,56 @@ sdkHelperAttachmentApp.provider('attachmentHelper', ['underscore', function (und
         };
     };
 }]);
+
+app.factory('resizeImageService', ['promiseService', 'underscore', function (promiseService, underscore) {
+    return function (imageOrUri, options) {
+        var _processImage = function (image) {
+            var canvas = document.createElement('canvas');
+            var ctx = canvas.getContext('2d');
+
+            options = underscore.defaults(options || {}, {
+                width: 80,
+                height: 80,
+                center: true,
+                crop: true,
+                output: 'image/png'
+            });
+
+            canvas.width = options.width;
+            canvas.height = options.height;
+
+            if (options.crop) {
+                var sX = 0, sY = 0;
+                var scaleToHeight = (((options.width * image.height) / options.height) > image.width);
+
+                var sW = (scaleToHeight ? Math.floor(image.width) : Math.floor((options.width * image.height) / options.height));
+                var sH = (scaleToHeight ? Math.floor((options.height * image.width) / options.width) : Math.floor(image.height));
+
+                if (options.center) {
+                    sX = (scaleToHeight ? 0 : Math.floor((sW - options.width) / 2));
+                    sY = (scaleToHeight ? Math.floor((sH - options.height) / 2) : 0);
+                }
+
+                ctx.drawImage(image, sX, sY, sW, sH, 0, 0, options.width, options.height);
+            } else {
+                ctx.drawImage(image, 0, 0, options.width, options.height);
+            }
+
+            return canvas.toDataURL(options.output, 1);
+        };
+
+        return promiseService.wrap(function (promise) {
+            if (typeof imageOrUri == 'string') {
+                var image = new Image();
+
+                loader.onload = function () {
+                    promise.resolve(_processImage(image));
+                };
+
+                loader.src = image;
+            } else {
+                promise.resolve(_processImage(imageOrUri));
+            }
+        });
+    };
+}]);
