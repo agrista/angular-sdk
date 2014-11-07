848d8dc6319d9a4576e482570977bdb321976612
Ross Savage <ross.savage@agrista.com>
2014-11-07 17:50:30 +0100

    refactor(connectionService): improve watchConnection, allow watch to be canceled

--- dist/agrista-mobile-sdk.js
+++ dist/agrista-mobile-sdk.js
@@ -7602,13 +7602,16 @@ cordovaCameraApp.factory('cameraService', ['promiseService', 'underscore', funct
     };
 }]);
 
-var cordovaConnectionApp = angular.module('ag.mobile-sdk.cordova.connection', []);
+var cordovaConnectionApp = angular.module('ag.mobile-sdk.cordova.connection', ['ag.sdk.library']);
 
-cordovaConnectionApp.factory('connectionService', ['$timeout', function ($timeout) {
+cordovaConnectionApp.factory('connectionService', ['$timeout', 'underscore', function ($timeout, underscore) {
     var _watchConnectionList = [];
     var _lastConnectionType = undefined;
+    var _timeout = undefined;
 
     var _updateConnection = function () {
+        $timeout.cancel(_timeout);
+
         if (navigator.connection && _lastConnectionType !== navigator.connection.type) {
             _lastConnectionType = navigator.connection.type;
 
@@ -7617,22 +7620,33 @@ cordovaConnectionApp.factory('connectionService', ['$timeout', function ($timeou
             });
         }
 
-        $timeout(_updateConnection, 10000);
+        _timeout = $timeout(_updateConnection, 10000);
     };
-
-    _updateConnection();
 
     return {
         watchConnection: function (callback) {
             if (typeof callback === 'function') {
                 _watchConnectionList.push(callback);
+
+                callback(_lastConnectionType);
+                _updateConnection();
+            }
+
+            return {
+                cancel: function () {
+                    _watchConnectionList = underscore.without(_watchConnectionList, callback);
+
+                    if (_watchConnectionList.length == 0) {
+                        $timeout.cancel(_timeout);
+                    }
+                }
             }
         },
         isOnline: function () {
-            return (navigator.connection.type !== Connection.NONE && navigator.connection.type !== Connection.UNKNOWN);
+            return (navigator.connection && navigator.connection.type !== Connection.NONE && navigator.connection.type !== Connection.UNKNOWN);
         },
         isMobile: function () {
-            return (navigator.connection.type === Connection.CELL ||
+            return navigator.connection && (navigator.connection.type === Connection.CELL ||
                 navigator.connection.type === Connection.CELL_2G ||
                 navigator.connection.type === Connection.CELL_3G ||
                 navigator.connection.type === Connection.CELL_4G);
