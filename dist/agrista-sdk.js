161aa417beead535eea47438fd6cc346f8705183
Sarah Kaindl <sarah.kaindl@agrista.com>
2014-12-09 14:32:57 +0100

    chore(build): build dist files

--- dist/agrista-sdk.js
+++ dist/agrista-sdk.js
@@ -824,6 +824,13 @@ sdkApiApp.factory('productionRegionApi', ['$http', '$log', 'pagingService', 'pro
                 }, promise.reject);
             });
         },
+        createProductionRegion: function (data) {
+            return promiseService.wrap(function(promise) {
+                $http.post(_host + 'api/subregion', data, {withCredentials: true}).then(function (res) {
+                    promise.resolve(res.data);
+                }, promise.reject);
+            });
+        },
         updateProductionRegion: function(region) {
             return promiseService.wrap(function(promise) {
                 $http.post(_host + 'api/subregion/' + region.id, region, {withCredentials: true}).then(function (res) {
