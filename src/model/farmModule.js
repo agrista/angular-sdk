703b7e465b054a4adddbaced69d45d9765e3d914
Ross Savage <ross.savage@agrista.com>
2016-09-28 16:16:52 +0200

    feat(Farm): adds Farm model

---
+++ src/model/farmModule.js
@@ -0,0 +1,37 @@
+var sdkModelFarm = angular.module('ag.sdk.model.farm', ['ag.sdk.library', 'ag.sdk.model.base']);
+
+sdkModelFarm.factory('Farm', ['inheritModel', 'Model', 'privateProperty', 'readOnlyProperty', 'underscore',
+    function (inheritModel, Model, privateProperty, readOnlyProperty, underscore) {
+        function Farm (attrs) {
+            Model.Base.apply(this, arguments);
+
+            this.data = (attrs && attrs.data) || {};
+
+            if (underscore.isUndefined(attrs) || arguments.length === 0) return;
+
+            this.id = attrs.id || attrs.$id;
+            this.name = attrs.name;
+            this.organizationId = attrs.organizationId;
+
+            // Models
+            this.organization = attrs.organization;
+        }
+
+        inheritModel(Farm, Model.Base);
+
+        Farm.validates({
+            name: {
+                required: true,
+                length: {
+                    min: 1,
+                    max: 255
+                }
+            },
+            organizationId: {
+                required: true,
+                numeric: true
+            }
+        });
+
+        return Farm;
+    }]);
