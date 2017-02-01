35b6b8b4cfb93cf7465df95fbd45351d5d6acd73
Ross Savage <ross.savage@agrista.com>
2017-02-01 08:20:00 +0100

    refactor(DesktopValuation): changes farmland-value-data element to farmland-value-table

--- dist/agrista-sdk.js
+++ dist/agrista-sdk.js
@@ -13126,7 +13126,7 @@ sdkModelDesktopValuationDocument.factory('DesktopValuation', ['Base', 'Comparabl
 
             var defaultReportBody = '<div class="tinymce-container pdf-container">' +
                 '<h2 id="property-description">Property Description</h2><br/><table id="property-description-table" width="100%"></table><br/>' +
-                '<h2 id="farmland-value">Estimated Farmland Value</h2><br/><div id="farmland-value-data"></div><br/>' +
+                '<h2 id="farmland-value">Estimated Farmland Value</h2><br/><div id="farmland-value-table"></div><br/>' +
                 '<h2 id="regional-value">Regional Value Development</h2><br/><div id="regional-value-graph"></div><br/>' +
                 '<h2 id="comparable-sales">Comparable Sales</h2><table id="comparable-sales-table" width="100%"></table><br/>' +
                 '<h2 id="disclaimer">Disclaimer</h2><p>Estimates of farmland and property value is based on the aggregation of regional sales data and assumptions regarding the property being valued.</p><br/><br/>' +
