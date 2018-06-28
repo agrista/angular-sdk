6555ff1a6ef58d55ee17d511feece32a27057c4e
Ross Savage <ross.savage@agrista.com>
2018-06-28 18:38:56 +0200

    refactor(Asset): adds general VME categories for Vehicles, Machinery & Equipment

--- dist/agrista-sdk.js
+++ dist/agrista-sdk.js
@@ -12286,15 +12286,18 @@ sdkModelAsset.factory('Asset', ['AssetBase', 'attachmentHelper', 'Base', 'comput
                 {category: 'Preharvest', subCategory: 'Pesticides', unit: 'l'}
             ],
             vme: [
+                {category: 'Vehicles'},
                 {category: 'Vehicles', subCategory: 'Bakkie'},
                 {category: 'Vehicles', subCategory: 'Car'},
                 {category: 'Vehicles', subCategory: 'Truck'},
                 {category: 'Vehicles', subCategory: 'Tractor'},
+                {category: 'Machinery'},
                 {category: 'Machinery', subCategory: 'Mower'},
                 {category: 'Machinery', subCategory: 'Mower Conditioner'},
                 {category: 'Machinery', subCategory: 'Hay Rake'},
                 {category: 'Machinery', subCategory: 'Hay Baler'},
                 {category: 'Machinery', subCategory: 'Harvester'},
+                {category: 'Equipment'},
                 {category: 'Equipment', subCategory: 'Plough'},
                 {category: 'Equipment', subCategory: 'Harrow'},
                 {category: 'Equipment', subCategory: 'Ridgers'},
@@ -12304,7 +12307,7 @@ sdkModelAsset.factory('Asset', ['AssetBase', 'attachmentHelper', 'Base', 'comput
                 {category: 'Equipment', subCategory: 'Combine'},
                 {category: 'Equipment', subCategory: 'Spreader'},
                 {category: 'Equipment', subCategory: 'Sprayer'},
-                {category: 'Equipment', subCategory: 'Mixer'},
+                {category: 'Equipment', subCategory: 'Mixer'}
             ]
         });
 
