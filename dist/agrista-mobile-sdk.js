c4646388677b4c703721e694a8a1b3721bd86f46
Ross Savage <ross.savage@agrista.com>
2019-10-23 19:00:25 +0200

    refactor(pipGeoApi): adds getPortionLandValues

--- dist/agrista-mobile-sdk.js
+++ dist/agrista-mobile-sdk.js
@@ -54,6 +54,9 @@ sdkApiGeoApp.factory('pipGeoApi', ['httpRequestor', 'configuration', 'pagingServ
 
             return httpRequestor(host + 'api/geo/portion' + (query ? '?' + query : ''));
         },
+        getPortionLandValues: function (params) {
+            return pagingService.page(host + 'api/geo/portion-values', trimQuery(params));
+        },
         searchPortions: function (params) {
             return pagingService.page(host + 'api/geo/portions', trimQuery(params));
         },
